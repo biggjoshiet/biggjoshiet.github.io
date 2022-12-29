adedca5ed12232b01d74843952e93de94de739af
biggjoshiet <josh.l.torrey@gmail.com>
2022-12-29 19:00:57 +0100

    updating with promises
    updating with promises

--- Examples/JS.js
+++ Examples/JS.js
@@ -487,6 +487,325 @@ console.log(nurseOlynyk.remainingVacationDays);
 nurseOlynyk.addCertification('Genetics');
 console.log(nurseOlynyk.certifications)
 
+/* class example of reduce */
+const Calculate = {
+  sum(inputArray) {
+    let totalSum = inputArray.reduce((x,y) => x + y);
+    
+    return totalSum;
+  }
+}
+
+/*-------------------------------------------------------*/
+/* module exmaples for importing/exporting by default */
+/*-------------------------------------------------------*/
+/* dom-functions.js */
+const toggleHiddenElement = (domElement) => {
+    if (domElement.style.display === 'none') {
+      domElement.style.display = 'block';
+    } else {
+      domElement.style.display = 'none';
+    }
+}
+ 
+const changeToFunkyColor = (domElement) => {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+ 
+  domElement.style.background = `rgb(${r}, ${g}, ${b})`;
+}
+ 
+const resources = { 
+  toggleHiddenElement, 
+  changeToFunkyColor
+}
+export default resources;
+
+//This default exports object can now be used within secret-messages.js like so:
+
+import domFunctions from '../modules/dom-functions.js';
+ 
+const { toggleHiddenElement, changeToFunkyColor } = domFunctions;
+ 
+const buttonElement = document.getElementById('secret-button');
+const pElement = document.getElementById('secret-p');
+ 
+buttonElement.addEventListener('click', () => {
+  toggleHiddenElement(pElement);
+  changeToFunkyColor(buttonElement);
+});
+
+// separate example with renaming imported functions
+/* inside greeterEspanol.js */
+const greet = () => {
+  console.log('hola');
+}
+export { greet };
+ 
+/* inside greeterFrancais.js */
+const greet = () => {
+  console.log('bonjour');
+}
+export { greet };
+Now, let’s say you wanted to use each of these functions in a program, called main.js, that simulates a conversation between a spanish-speaker and a french-speaker.
+
+import { greet } from 'greeterEspanol.js';
+import { greet } from 'greeterFrancais.js';
 
 
+//Error handling
+//
+//
+//index.js
+const Calculate = {
+  sum(inputArray) {
+    if (inputArray.length === 0){
+      return 0;
+    }
+    return inputArray.reduce((sum, value) => {
+      return sum + value;
+    })
+  }
+}
 
+module.exports = Calculate;
+//index_test.js
+const assert = require('assert');
+const Calculate =  require('../index.js')
+
+describe('Calculate', () => {
+  describe('.sum',() => {
+    it('returns the sum of an array of numbers', () => {
+      const expectedResult = 6;
+      const inputArray = [1,2,3]
+      
+      const result = Calculate.sum(inputArray)
+      
+      assert.strictEqual(result, expectedResult);
+    });
+    
+    it('returns the sum of a four item list', ()=>{
+      const expectedResult = 22;
+      const inputArray = [4,6,5,7];
+      
+      const result = Calculate.sum(inputArray);
+      
+      assert.strictEqual(result, expectedResult)
+      
+    });
+    it('returns zero for an empty array', ()=>{
+      const expectedResult = 0;
+      const inputArray = [];
+      
+      const result = Calculate.sum(inputArray);
+      
+      assert.strictEqual(result, expectedResult)
+      
+    });
+  });
+});
+
+/*-------------------------------------------------------*/
+/* testing */
+/*-------------------------------------------------------*/
+// needs to fill 
+
+/*-------------------------------------------------------*/
+// Promises, chained promises, thens, and catches 
+/*-------------------------------------------------------*/
+
+//promise and catch in js, followed by accompanying html code
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+checkInventory(order)
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+ return processPayment(resolvedValueArray);
+})
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+  return shipOrder(resolvedValueArray);
+})
+.then((successMessage) => {
+  console.log(successMessage);
+})
+.catch((errorMessage) => {
+  console.log(errorMessage);
+});
+
+
+/*
+html code for promise/then/catch
+const store = {
+  sunglasses: {
+    inventory: 817, 
+    cost: 9.99
+  },
+  pants: {
+    inventory: 236, 
+    cost: 7.99
+  },
+  bags: {
+    inventory: 17, 
+    cost: 12.99
+  }
+};
+
+const checkInventory = (order) => {
+  return new Promise ((resolve, reject) => {
+   setTimeout(()=> {  
+   const itemsArr = order.items;  
+   let inStock = itemsArr.every(item => store[item[0]].inventory >= item[1]);
+   
+   if (inStock){
+     let total = 0;   
+     itemsArr.forEach(item => {
+       total += item[1] * store[item[0]].cost
+     });
+     console.log(`All of the items are in stock. The total cost of the order is ${total}.`);
+     resolve([order, total]);
+   } else {
+     reject(`The order could not be completed because some items are sold out.`);
+   }     
+}, generateRandomDelay());
+ });
+};
+
+const processPayment = (responseArray) => {
+  const order = responseArray[0];
+  const total = responseArray[1];
+  return new Promise ((resolve, reject) => {
+   setTimeout(()=> {  
+   let hasEnoughMoney = order.giftcardBalance >= total;
+   // For simplicity we've omited a lot of functionality
+   // If we were making more realistic code, we would want to update the giftcardBalance and the inventory
+   if (hasEnoughMoney) {
+     console.log(`Payment processed with giftcard. Generating shipping label.`);
+     let trackingNum = generateTrackingNumber();
+     resolve([order, trackingNum]);
+   } else {
+     reject(`Cannot process order: giftcard balance was insufficient.`);
+   }
+   
+}, generateRandomDelay());
+ });
+};
+
+
+const shipOrder = (responseArray) => {
+  const order = responseArray[0];
+  const trackingNum = responseArray[1];
+  return new Promise ((resolve, reject) => {
+   setTimeout(()=> {  
+     resolve(`The order has been shipped. The tracking number is: ${trackingNum}.`);
+}, generateRandomDelay());
+ });
+};
+
+
+// This function generates a random number to serve as a "tracking number" on the shipping label. In real life this wouldn't be a random number
+function generateTrackingNumber() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+// This function generates a random number to serve as delay in a setTimeout() since real asynchrnous operations take variable amounts of time
+function generateRandomDelay() {
+  return Math.floor(Math.random() * 2000);
+}
+
+module.exports = {checkInventory, processPayment, shipOrder};
+*/
+
+
+/* example of Promise.all */
+const {checkAvailability} = require('./library.js');
+
+const onFulfill = (itemsArray) => {
+  console.log(`Items checked: ${itemsArray}`);
+  console.log(`Every item was available from the distributor. Placing order now.`);
+};
+
+const onReject = (rejectionReason) => {
+  console.log(rejectionReason);
+};
+
+// Write your code below:
+
+const checkSunglasses = checkAvailability('sunglasses', 'Favorite Supply Co.');
+const checkPants = checkAvailability('pants', 'Favorite Supply Co.'); 
+const  checkBags = checkAvailability('bags', 'Favorite Supply Co.');
+
+Promise.all([checkSunglasses, checkPants, checkBags])
+  .then(onFulfill)
+  .catch(onReject);
+
+/*
+accompanying html code for promise.all example
+const checkAvailability = (itemName, distributorName) => {
+    console.log(`Checking availability of ${itemName} at ${distributorName}...`);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (restockSuccess()) {
+                console.log(`${itemName} are in stock at ${distributorName}`)
+                resolve(itemName);
+            } else {
+                reject(`Error: ${itemName} is unavailable from ${distributorName} at this time.`);
+            }
+        }, 1000);
+    });
+};
+
+module.exports = { checkAvailability };
+
+
+// This is a function that returns true 80% of the time
+// We're using it to simulate a request to the distributor being successful this often
+function restockSuccess() {
+    return (Math.random() > .2);
+}
+*/
+/*-------------------------------------------------------*/
+/* comparison of callback functions, Promises, and then */
+/* async... await */
+/*-------------------------------------------------------*/
+  const fs = require('fs');
+const promisifiedReadfile = require('./promisifiedReadfile');
+      
+// Here we use fs.readfile() and callback functions:
+fs.readFile('./file.txt', 'utf-8', (err, data) => {
+  if (err) throw err;
+  let firstSentence = data;
+  fs.readFile('./file2.txt',  'utf-8', (err, data) => {
+    if (err) throw err;
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence);
+  });
+});
+
+// Here we use native promises with our "promisified" version of readfile:
+let firstSentence;
+promisifiedReadfile('./file.txt', 'utf-8')
+  .then((data) => {
+    firstSentence = data;
+    return promisifiedReadfile('./file2.txt', 'utf-8');
+  })
+  .then((data) => {
+    let secondSentence = data;
+    console.log(firstSentence, secondSentence)
+  })
+  .catch((err) => {console.log(err)});
+
+// Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
+async function readFiles() {
+  let firstSentence = await promisifiedReadfile('./file.txt', 'utf-8');
+  let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8');
+  console.log(firstSentence, secondSentence);
+}
+
+readFiles();
